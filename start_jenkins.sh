f554d1fa142d0527da7b0192ecb78a31c009dcc0
Patrick White <whitep@vcu.edu>
2015-11-08 17:34:33 -0500

    ignoring local data

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker run --name jenkins \
+sudo docker run --name jenkins \
 	-p 8080:8080 \
 	-p 50000:50000 \
-	-v /opt/jenkins/data:/var/jenkins_home \
+	-v $(pwd)/data:/var/jenkins_home \
 	jenkins
